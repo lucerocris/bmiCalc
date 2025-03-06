27f3532009d0026a981e3313f7653fe9469eda13
Cris Lucero <lucerocris22@gmail.com>
2025-03-06 14:54:07 +0800

    Metric BMI calculation implemented

--- src/script.js
+++ src/script.js
@@ -1,17 +1,38 @@
 const radioButton = document.querySelectorAll('input[name="unit"]');
+const cmInput = document.getElementById('cmInput');
+const kgInput = document.getElementById('kgInput');
+let isImperial = false;
+let metHeight = 0;
+let metWeight = 0;
 
 radioButton.forEach((radio) => {
   radio.addEventListener('change', checkClick);
 });
+
+cmInput.addEventListener('input', updateHeight);
+kgInput.addEventListener('input', updateWeight);
 
 function checkClick(event) {
   const radio = event.target;
   if (radio.checked) {
     hideBorder(radio.value);
     showImperial(radio.value);
-  } else {
-    hideBorder(radio.value);
-    showImperial(radio.value);
+    isImperial = radio.value === 'imperial';
+    console.log('isImperial:', isImperial);
+  }
+}
+
+function updateHeight(e) {
+  if (!isImperial) {
+    metHeight = parseFloat(e.target.value);
+    calculateBMI();
+  }
+}
+
+function updateWeight(e) {
+  if (!isImperial) {
+    metWeight = parseFloat(e.target.value);
+    calculateBMI();
   }
 }
 
@@ -49,5 +70,16 @@ function showImperial(unit) {
     inputContainer.classList.add('lg:flex-row');
     heightUnitText.innerHTML = 'cm';
     weigthUnitText.innerHTML = 'kg';
+  }
+}
+
+function calculateBMI() {
+  if (!isImperial) {
+    if (metHeight > 0 && metWeight > 0) {
+      const heightInMeters = metHeight / 100;
+      const bmi = metWeight / (heightInMeters * heightInMeters);
+      bmi.toFixed(1);
+      console.log(`BMI: ${bmi}`);
+    }
   }
 }
